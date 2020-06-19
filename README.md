53306b81653fc88ce0a9694ea1fdd6078562668d
TytoTF08 <tytotf08@github.com>
2020-06-19 08:52:22 -0400

    create the first scripting sheet

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# xcelerate.js
+# xCelerate JS
+
+a Javascript framework which lets you create a webpage - with only a few lines of JScript code. You create an index.html page with just the following structure:
+```html
+<!DOCTYPE html>
+<html>
+<head>
+  <script src = "dominat.us/xcelerate.js"></script>
+</head>
+<body>
+</body>
+```
+and of course, some custom HTML if you'd like.
+You should also create two files: main.js and style.css. xCelerate JS looks for these files when the page starts. They can be empty, but are system requirements for xCelerate.
---
+++ xcelerate.js
@@ -0,0 +1,52 @@
+// xCelerate JS Scripting
+
+var randomContent;
+function print(styling, content) {
+	if (typeof styling === "string" && typeof content === "string" && styling !== "" && content !== "") {
+		document.body.innerHTML += `<p style = "` + styling +`">` + content + `</p>`
+	}
+}
+function hello(outPutEnvironment) {
+	if (typeof outPutEnvironment === "string") {
+		if (outPutEnvironment === "console") {
+			console.log("Hello");
+		} else if (outPutEnvironment === "html") {
+			document.body.innerHTML += `<p>Hello</p>`;
+		} else if (outPutEnvironment === "alert") {
+			alert("Hello");
+		} else if (outPutEnvironment === "") {
+			alert("SyntaxError: must specify at least one output environment.");
+		} else {
+			alert("SyntaxError: '"+outPutEnvironment+"' is not a valid output environment.");
+		}
+	} else {
+		alert("SyntaxError: output environment must be a specified string.");
+	}
+}
+function echo(content, outPutEnvironment) {
+	if (typeof outPutEnvironment === "string") {
+		if (outPutEnvironment === "console") {
+			console.log(content);
+		} else if (outPutEnvironment === "html") {
+			document.body.innerHTML += `<p>` + content + `</p>`;
+		} else if (outPutEnvironment === "alert") {
+			alert(content);
+		} else if (outPutEnvironment === "") {
+			alert("SyntaxError: must specify at least one output environment.");
+		} else {
+			alert("SyntaxError: '"+outPutEnvironment+"' is not a valid output environment.");
+		}
+	} else {
+		alert("SyntaxError: output environment must be a specified string.");
+	}
+}
+function random(arrayName) {
+	if (typeof arrayName === "string") {
+		randomContent = arrayName[Math.floor(Math.random() * arrayName.length)];
+		document.body.innerHTML += `<p>` + randomContent + `</p>`;
+	}	
+}
+window.onload = function communicate() {
+	document.body.innerHTML += `<script src = "main.js"></script>`;
+	document.querySelector("head").innerHTMl += `<link rel = "stylesheet" href = "style.css"/>`
+}
